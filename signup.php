9b13071c2779f986a1948364d8987e88ec64ba5b
Andre Irawan <andreirawan97@gmail.com>
2019-03-08 11:03:07 +0700

    Add favicon

--- signup.php
+++ signup.php
@@ -3,6 +3,7 @@
 <head>  
   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
   <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Go Signup!</title>
