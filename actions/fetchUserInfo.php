176ad1f1de24099f2b85b2c854472d999c770dbb
Andre Irawan <andreirawan97@gmail.com>
2019-03-08 14:46:12 +0700

    Fixing error when invalid id

--- actions/fetchUserInfo.php
+++ actions/fetchUserInfo.php
@@ -7,5 +7,14 @@
   $result = $conn->query($query);
   $row = mysqli_fetch_assoc($result);
 
-  echo json_encode(array("id" => "$row[id]", "email" => "$row[email]", "fullName" => "$row[fullname]"))
+  if(is_array($row)){
+    echo json_encode($row);
+  }
+  else{
+    echo json_encode(array("status" => "err"));
+  }
+
+  /*
+    Dia ngirim dari hasilnya berupa json
+  */
 ?>
